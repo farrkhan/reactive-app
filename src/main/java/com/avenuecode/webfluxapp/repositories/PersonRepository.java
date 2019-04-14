6a3f9816c3bddafffaf28947ec00cc2d56a6a5b0
farrkhan <farrukh.khan@capgemini.com>
2019-04-14 18:55:06 +0530

    Delete PersonRepository.java

--- src/main/java/com/avenuecode/webfluxapp/repositories/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.avenuecode.webfluxapp.repositories;
-
-import com.avenuecode.webfluxapp.domain.Person;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends ReactiveMongoRepository<Person, String> {
-}
